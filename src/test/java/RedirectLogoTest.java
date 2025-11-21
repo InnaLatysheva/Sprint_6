74127aab0d79b4ad5691dc14ef61bebd8cf54be1
Inna Latysheva <titowa1993@yandex.ru>
2025-11-21 23:16:59 +0300

    Внесла правки по результатам ревью. Пересобрала пул реквест, в который попали проверки по работе учебного сервиса Яндекс. Самоката

---
+++ src/test/java/RedirectLogoTest.java
@@ -0,0 +1,47 @@
+import configure.EnvConfig;
+import driverRule.DriverRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import pages.MainPage;
+import pages.YandexPage;
+
+public class RedirectLogoTest {
+    String text = "Hello, world!";
+
+    @Rule
+    public DriverRule driverRule = new DriverRule();
+
+    @Test()
+    public void redirectToYandex() {
+        WebDriver driver = driverRule.getDriver();
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.open()
+                .addCookie(EnvConfig.COOKIE_NAME_1, EnvConfig.COOKIE_VALUE)
+                .addCookie(EnvConfig.COOKIE_NAME_2, EnvConfig.COOKIE_NAME_2)
+                .open();
+
+        YandexPage ya = mainPage.clickOnYandex();
+
+        ya.switchToNewTab()
+                .enterSomeText(text);
+
+    }
+
+    @Test
+    public void redirectToSamokat() {
+        WebDriver driver = driverRule.getDriver();
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.open()
+                .addCookie(EnvConfig.COOKIE_NAME_1, EnvConfig.COOKIE_VALUE)
+                .addCookie(EnvConfig.COOKIE_NAME_2, EnvConfig.COOKIE_NAME_2)
+                .open()
+                .clickOnSamokat()
+                .checkRedirectToSamokat();
+    }
+
+
+
+}
